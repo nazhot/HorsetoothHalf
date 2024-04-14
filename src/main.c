fc8a53e62ba38ae6a31400c0fe1436f7a5bdba90
nazhot <15216154+nazhot@users.noreply.github.com>
2024-04-14 06:18:32 -0600

    Reading Lines of Files, Finding Header Line

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main( int argc, char *argv[] ) {
     FILE *inputFile;
@@ -11,6 +12,20 @@ int main( int argc, char *argv[] ) {
             fprintf( stderr, "Cannot open %s\n", inputFileNames[i] );
             continue;
         }
+        size_t lineSize = 1024;
+        char *lineBuffer;
+
+        while ( getline( &lineBuffer, &lineSize, inputFile )  != -1 ) {
+            if ( !lineBuffer ) {
+                fprintf( stderr, "Cannot get line of file %s\n", inputFileNames[i] );
+                continue;
+            }
+            if ( !strncmp( lineBuffer, "Place", 5 ) ) {
+                printf( "%s: %s", inputFileNames[i], lineBuffer );
+            }
+        }
+
+        free( lineBuffer );
     }
 
     return 0;
