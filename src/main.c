ae30906e1de431eacfc5eba807c60c29df2faa3c
nazhot <15216154+nazhot@users.noreply.github.com>
2024-04-15 16:26:07 -0600

    Working CSV Output

--- src/main.c
+++ src/main.c
@@ -2,9 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+uint splitLine( uint numSplits, uint splitLength, char splitArray[numSplits][splitLength], const char *line, const char delimeter ) {
+    uint splitArrayIndex = 0;
+    uint splitArrayIndexIndex = 0;
+    for ( uint i = 0; line[i]; ++i ) {
+        if ( line[i] == delimeter ) {
+            continue;
+        } else if ( i != 0 && line[i - 1] == delimeter ) {
+            splitArrayIndex++;
+            splitArrayIndexIndex = 0;
+        }
+        splitArray[splitArrayIndex][splitArrayIndexIndex++] = line[i];
+    }
+    return splitArrayIndex;
+}
+
 int main( int argc, char *argv[] ) {
     FILE *inputFile;
-    char *inputFileNames[5] = { "2018.txt", "2019.txt", "2021.txt", "2022.txt", "2023.txt" };
+    char *inputFileNames[5] = { "2018", "2019", "2021", "2022", "2023" };
+
+    FILE *outputFile = fopen( "results.csv", "w" );
+    if ( !outputFile ) {
+        fprintf( stderr, "Cannot open output file\n" );
+        exit( 1 );
+    }
+    fprintf( outputFile, "Year, Pace (seconds), Total Time (seconds), Total Time (minutes)\n" );
 
     for ( uint i = 0; i < 5; ++i ) {
         inputFile = fopen( inputFileNames[i], "r" );
@@ -13,20 +36,80 @@ int main( int argc, char *argv[] ) {
             continue;
         }
         size_t lineSize = 1024;
-        char *lineBuffer;
+        char *lineBuffer = NULL;
+        uint netTimeColumn = 0;
+
+        //find the line with the column headers
+        while ( getline( &lineBuffer, &lineSize, inputFile )  != -1 ) {
+            if ( !lineBuffer ) {
+                fprintf( stderr, "Cannot get line of file %s\n", inputFileNames[i] );
+                continue;
+            }
+            if ( strncmp( lineBuffer, "Place", 5 ) ) {
+                continue;
+            }
+            printf( "%s: %s", inputFileNames[i], lineBuffer );
+            char splitArray[20][256] = {0};
+            uint splitSize = splitLine( 20, 256, splitArray, lineBuffer, ' ' );
+            printf( "Size: %u\n", splitSize );
+            for ( uint j = 0; j < splitSize; ++j ) {
+                printf( "%u: %s\n", j, splitArray[j] );
+                if ( !strncmp( splitArray[j], "Nettime", 7 ) ) {
+                    netTimeColumn = j;
+                    break;
+                }
+            }
+            break;
+        }
+
+        getline( &lineBuffer, &lineSize, inputFile ); //skip line of ====='s
 
         while ( getline( &lineBuffer, &lineSize, inputFile )  != -1 ) {
             if ( !lineBuffer ) {
                 fprintf( stderr, "Cannot get line of file %s\n", inputFileNames[i] );
                 continue;
             }
-            if ( !strncmp( lineBuffer, "Place", 5 ) ) {
-                printf( "%s: %s", inputFileNames[i], lineBuffer );
+            char splitArray[20][256] = {0};
+            uint splitSize = splitLine( 20, 256, splitArray, lineBuffer, ' ' );
+            if ( splitSize == 0 ) {
+                continue;
             }
+            char *paceString = splitArray[splitSize - 1];
+            int paceStringIndex = strlen( paceString ) - 1;
+            uint paceSeconds = 0;
+
+            //seconds
+            paceSeconds += paceString[paceStringIndex--] - 48;
+            paceSeconds += ( paceString[paceStringIndex--] - 48 ) * 10;
+            paceStringIndex--;
+
+            //minutes
+            uint minuteMultiplier = 1;
+            while ( paceStringIndex > -1 && paceString[paceStringIndex] != ':' ) {
+                paceSeconds += ( paceString[paceStringIndex--] - 48 ) * 60 * minuteMultiplier;
+                minuteMultiplier *= 10;
+            }
+
+            paceStringIndex--;
+
+            //hours
+            uint hourMultiplier = 1;
+            while ( paceStringIndex > -1 && paceString[paceStringIndex] != ':' ) {
+                paceSeconds += ( paceString[paceStringIndex--] - 48 ) * 3600 * hourMultiplier;
+                hourMultiplier *= 10;
+            }
+
+            uint timeSeconds = paceSeconds * 13.1;
+
+            fprintf( outputFile, "%s, %u, %u, %.2f\n",  inputFileNames[i], paceSeconds, timeSeconds, timeSeconds * 1.0 / 60.0 );
+
         }
 
         free( lineBuffer );
+        fclose( inputFile );
     }
+
+    fclose( outputFile );
 
     return 0;
 }
